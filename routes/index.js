e45f02cf3e67b59d0dcac7d4b5929da94b6de112
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-01-21 21:25:00 +0530

    set up the front-end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("home");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get('/register', (req, res) => {
+res.render('register');
 });
 
 module.exports = router;
